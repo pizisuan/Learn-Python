3df708d062291d34133e487bada1e6698598ebce
Pizi_Suan <math112358@sina.com>
2017-07-26 17:09:09 +0800

    Update Word segmentation.py

--- Word segmentation.py
+++ Word segmentation.py
@@ -12,3 +12,20 @@ items.sort(key=lambda x:x[1],reverse=True)
 for i in range(15):
 	word, count = items[i]
 	print("{0:<10}{1:>5}".format(word, count)) 
+
+# 结果如下：
+# 曹操          936
+# 孔明          831
+# 将军          772
+# 却说          656
+# 玄德          570
+# 关公          509
+# 丞相          491
+# 二人          466
+# 不可          441
+# 荆州          421
+# 不能          387
+# 孔明曰        385
+# 玄德曰        383
+# 如此          378
+# 张飞          348
